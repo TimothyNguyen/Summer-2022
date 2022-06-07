2e0b44bb546a86558c3d420d2332dcaf2515ccc2
Timothy Nguyen <quynhthoa1972@gmail.com>
2022-06-07 16:52:04 -0400

    Factory design patterns

---
+++ DesignPatterns/ChallengeFactoryPattern.js
@@ -0,0 +1,30 @@
+function ToyFactory() {
+    this.toy = ToyDuck; //toy property set to ToyDuck by default
+    this.createToy = function(toyChosen) {
+        if(toyChosen.toyType === "car") {
+            this.toy = ToyCar;
+        } else if (toyChosen.toyType == "duck") {
+            this.toy = ToyDuck;
+        } 
+        return new this.toy(toyChosen);
+    }
+}
+
+function ToyDuck(toyObj) {
+    this.color = toyObj.color;
+    this.price = toyObj.price;
+} //define the ToyDuck class 
+
+function ToyCar(toyObj) {
+    this.color = toyObj.color;
+    this.price = toyObj.price;
+    this.name = toyObj.name;
+}
+
+var toyFactory = new ToyFactory();
+var car = toyFactory.createToy({
+            toyType : "car",
+            color: "blue",
+            price : 12,
+            name : "honda"
+})
